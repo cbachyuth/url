4b3e1b3f7f61b7572f57d7e6f4ded19a249eb5b5
Aditya Choudhary <109758988+AadiX11@users.noreply.github.com>
2022-12-01 12:38:34 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -580,7 +580,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
-        elif query.data == "rfrsh":
+    elif query.data == "rfrsh":
         await query.answer("Fetching MongoDb DataBase")
         buttons = [[
             InlineKeyboardButton('⟸ Bᴀᴄᴋ', callback_data='about'),
