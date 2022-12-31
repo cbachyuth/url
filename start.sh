78057c85b4cd0d4f1ac7573236dfcfff2f3c1fab
Anubisop17 <89119252+Anubisop17@users.noreply.github.com>
2023-01-01 00:30:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Rcbforever111/url /Eva
+  git clone https://github.com/Anubisop17/url /Eva
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Eva
